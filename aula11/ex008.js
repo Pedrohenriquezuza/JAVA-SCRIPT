d522218f0bf61c6612819f088994bc8b2cedb9eb
Pedro Henrique Zuza <156633255+Pedrohenriquezuza@users.noreply.github.com>
2024-04-21 15:11:43 -0300

    atualizei os ex de javascript

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,8 +1,6 @@
-var vel = 58
-console.log(`A velocidade do seu carro é de ${vel}Km/h`)
-if (vel > 60){
-    console.log('Você Ultrapassou a velocidade permitida, MULTADO!')
-} else {
-    console.log('Parabens, você esta na velocidade permitida!')
+var vel = 20;
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel >= 60){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO')
 }
-console.log('dirija sempre usando cinto de segurança')
+console.log(`Dirija sempre usando cinto de segurança`) 
